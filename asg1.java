b34164cbea474692772669c1342e0a5f95649c4d
bmanisha12 <b.manisha59@gmail.com>
2016-03-15 15:20:25 +0530

    Added files via upload

---
+++ asg1.java
@@ -0,0 +1,33 @@
+
+public class asg1 {
+	public static void main(String args[])
+	{
+		int ans, tc, a=10;
+		float b = 2.5f;
+		tc = (int)b;
+		ans = a + tc; 
+		System.out.println("\n Addition = " +ans);
+	}
+
+}
+/*
+output:
+/*
+output:
+ /*
+ output:
+ /*
+ output:
+ Addition = 12
+
+ */	
+	
+	
+
+ */	
+	
+	
+
+*/	
+	
+	
